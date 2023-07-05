5ffbd5c944b82fcd2182da35a73ee5ed79b1df9c
kostas <kostasp@icloud.com>
2023-07-05 18:26:09 +0200

    new validation animations

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,46 +1,80 @@
 
 const form = document.getElementById("form");
 const userName = document.getElementById("name");
+const labelName = document.getElementById("label-name");
 const userMail = document.getElementById("mail");
-const userPhone = document.getElementById("phone");
+const msgText = document.getElementById("msg");
+const labelMail = document.getElementById("label-mail");
+const labelMsg = document.getElementById("label-message");
+const requiredLabel = document.getElementById("required-label");
+
+
 const submitBut = document.getElementById("submit-button");
 
-const overlay = document.getElementById("overlay");
-const overlayContent = document.getElementById("overlay-content");
+
 
 const validate = (e) => {
     e.preventDefault();
     if (userName.value === ""){
-        showOverlay("Please enter your full name");
-        userName.focus();
+        userName.placeholder = "Please enter your full name"
+        userName.classList.add("error");
+        userName.classList.add("error-label");
+        labelName.classList.add("error-label");
+        requiredLabel.classList.add("error-label");
         return;
     }
     if (userMail.value === ""){
-        showOverlay("Please enter your email address.");
-        userMail.focus();
+        userMail.placeholder = "Please enter your email address.";
+        userMail.classList.add("error");
+        userMail.classList.add("error-label");
+        labelMail.classList.add("error-label");
+        requiredLabel.classList.add("error-label");
         return;
     }
     if (!emailIsValid(userMail.value)) {
-        showOverlay("Please enter a valid email address.");
-        userMail.focus();
+        userMail.placeholder = "Please enter a valid email address.";
+        userMail.value = "";
+        userMail.classList.add("error");
+        userMail.classList.add("error-label");
+        labelMail.classList.add("error-label");
+        requiredLabel.classList.add("error-label");
+        return;
+      }
+      
+      if (msgText.value.length <= 0){
+        msgText.placeholder = "Please message cannot be empty...";
+        msgText.classList.add("error");
+        msgText.classList.add("error-label");
+        labelMsg.classList.add("error-label");
+        requiredLabel.classList.add("error-label");
         return;
       }
 
     form.submit();
     form.reset();
-}
+};
 
 const emailIsValid = email => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  }
-  const showOverlay = (message) => {
-    overlayContent.textContent = message;
-    overlay.style.display = "block";
   };
   
-  const hideOverlay = () => {
-    overlay.style.display = "none";
+  
+  
+  const hideError = () => {
+    userMail.placeholder = "Enter your email address";
+    userName.placeholder = "Enter your full name";
+    msgText.placeholder = "Write your message here..."
+    userMail.classList.remove("error");
+    userMail.classList.remove("error-label");
+    labelMail.classList.remove("error-label");
+    userName.classList.remove("error");
+    userName.classList.remove("error-label");
+    labelName.classList.remove("error-label");
+    msgText.classList.remove("error");
+    msgText.classList.remove("error-label");
+    labelMsg.classList.remove("error-label");
+    requiredLabel.classList.remove("error-label");
   };
 
 form.addEventListener("submit", validate);
-overlay.addEventListener("click", hideOverlay);
+document.addEventListener("click", hideError);
